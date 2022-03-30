6b3c06676cc5f615692dca5541d992ff22fe482e
Steve <stevealv@hotmail.com>
2022-03-29 21:00:55 -0600

    [Update] Sign-up view
    Form student.

--- Booking/src/app/sign-up/sign-up.component.ts
+++ Booking/src/app/sign-up/sign-up.component.ts
@@ -7,9 +7,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignUpComponent implements OnInit {
 
+  isVisible = false;
+
   constructor() { }
 
   ngOnInit(): void {
+    //Make visible the students forms.
+    // $('#select').on('change', function () {
+    //   $(function () { $("#hide").fadeIn(500); });
+    // });
+  }
+
+  showStudentForm() {
+
+    let student = (<HTMLInputElement>document.getElementById('select')).value;
+    console.log(student);
+    if (student == "1") {
+      this.isVisible = true;
+    } else if (student == "2") {
+      this.isVisible = false;
+    }
+
+
+
   }
 
 }
