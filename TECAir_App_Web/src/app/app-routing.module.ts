2caf00e1127effd79e65a3b471e65e42b786fdcf
Steve <stevealv@hotmail.com>
2022-03-31 10:48:37 -0600

    [Add] Booking migration

--- TECAir_App_Web/src/app/app-routing.module.ts
+++ TECAir_App_Web/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "", pathMatch: "full", redirectTo: "/sign-up" },
+  { path: "sign-up", component: SignUpComponent },
+  { path: "login", component: LoginComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ TECAir_App_Web/src/app/sign-up/sign-up.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-sign-up',
+  templateUrl: './sign-up.component.html',
+  styleUrls: ['./sign-up.component.css']
+})
+export class SignUpComponent implements OnInit {
+
+  isVisible = false;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  showStudentForm() {
+
+    let student = (<HTMLInputElement>document.getElementById('select')).value;
+    console.log(student);
+    if (student == "1") {
+      this.isVisible = true;
+    } else if (student == "2") {
+      this.isVisible = false;
+    }
+
+
+
+  }
+
+}
