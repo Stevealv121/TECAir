de2bbad6649c6f97a8c8284a17d54bb60d6183cc
Sebas <sebas0163@hotmail.com>
2022-04-04 22:12:32 -0600

    [Add] Population Scripts

---
+++ Scripts BD/FLIGHT_Population.sql
@@ -0,0 +1,3 @@
+INSERT INTO public."FLIGHT"(
+	"Id", "boarding Gate", "Price", "Status", "Route Code", "Airplane Plate")
+	VALUES (1, 2, 200, TRUE, 1, 'NGB-201');
---
+++ Scripts BD/ROLE_Population.sql
@@ -0,0 +1,4 @@
+INSERT INTO public."ROLE"(
+	"Name", "Description")
+	VALUES ('Student', 'Student that belongs to an University and has a special price'),
+	('Worker', 'Airline employee'), ('Client','Common client');
