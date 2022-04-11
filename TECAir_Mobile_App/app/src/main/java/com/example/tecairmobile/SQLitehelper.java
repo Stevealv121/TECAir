820e62baa569bc7a2896b2056a6f49c7a0791d79
Dennis Jimenez <alejimenezc@hotmail.com>
2022-04-11 14:02:57 -0600

    [Add] Utilities

--- TECAir_Mobile_App/app/src/main/java/com/example/tecairmobile/SQLitehelper.java
+++ TECAir_Mobile_App/app/src/main/java/com/example/tecairmobile/SQLitehelper.java
@@ -6,20 +6,10 @@ import android.database.sqlite.SQLiteOpenHelper;
 
 import androidx.annotation.Nullable;
 
+import com.example.tecairmobile.Utilities.Utilities;
+
 public class SQLitehelper extends SQLiteOpenHelper {
 
-    final String CREATE_AIRPLANE_TABLE = "CREATE TABLE airplanes (plate TEXT, model TEXT)";
-    final String CREATE_APTO_TABLE = "CREATE TABLE applies_to (flight_id INTEGER, promotion_code INTEGER, final_price INTEGER)";
-    final String CREATE_BAGGAGE_TABLE = "CREATE TABLE baggage (id INTEGER, color TEXT, weight INTEGER)";
-    final String CREATE_BOOKS_TABLE = "CREATE TABLE books (user_id INTEGER, flight_id INTEGER)";
-    final String CREATE_FLIGHTS_TABLE = "CREATE TABLE flights (id INTEGER, boarding_gate INTEGER, price INTEGER, status BOOLEAN, route_code INTEGER, airplane_plate TEXT)";
-    final String CREATE_FLIGHTSTO_TABLE = "CREATE TABLE stopover (flight_id INTEGER, stopover TEXT)";
-    final String CREATE_HAS_TABLE = "CREATE TABLE has (baggage_id INTEGER, user_id INTEGER, price INTEGER)";
-    final String CREATE_PROMOTION_TABLE = "CREATE TABLE promotions (promotion_code INTEGER, description TEXT, title TEXT, day INTEGER, month INTEGER, year INTEGER, discount INTEGER)";
-    final String CREATE_ROLE_TABLE = "CREATE TABLE role (name TEXT, description TEXT)";
-    final String CREATE_ROUTE_TABLE = "CREATE TABLE route (route_code INTEGER, origin TEXT, destination TEXT, year INTEGER, month INTEGER, day INTEGER, hours INTEGER, minutes INTEGER)";
-    final String CREATE_SEATS_TABLE = "CREATE TABLE seats (id INTEGER, description TEXT, state BOOLEAN, user_id INTEGER, airplane_plate TEXT)";
-    final String CREATE_USER_TABLE = "CREATE TABLE user (id INTEGER, email TEXT, first_name TEXT, second_name TEXT, first_surname TEXT, second_surname TEXT, phone INTEGER, university TEXT, student_id INTEGER, role_name TEXT)";
 
     public SQLitehelper(@Nullable Context context, @Nullable String name, @Nullable SQLiteDatabase.CursorFactory factory, int version) {
         super(context, name, factory, version);
@@ -28,18 +18,18 @@ public class SQLitehelper extends SQLiteOpenHelper {
     @Override
     public void onCreate(SQLiteDatabase db) {
 
-        db.execSQL(CREATE_AIRPLANE_TABLE);
-        db.execSQL(CREATE_APTO_TABLE);
-        db.execSQL(CREATE_BAGGAGE_TABLE);
-        db.execSQL(CREATE_BOOKS_TABLE);
-        db.execSQL(CREATE_FLIGHTS_TABLE);
-        db.execSQL(CREATE_FLIGHTSTO_TABLE);
-        db.execSQL(CREATE_HAS_TABLE);
-        db.execSQL(CREATE_PROMOTION_TABLE);
-        db.execSQL(CREATE_ROLE_TABLE);
-        db.execSQL(CREATE_ROUTE_TABLE);
-        db.execSQL(CREATE_SEATS_TABLE);
-        db.execSQL(CREATE_USER_TABLE);
+        db.execSQL(Utilities.CREATE_AIRPLANE_TABLE);
+        db.execSQL(Utilities.CREATE_APTO_TABLE);
+        db.execSQL(Utilities.CREATE_BAGGAGE_TABLE);
+        db.execSQL(Utilities.CREATE_BOOKS_TABLE);
+        db.execSQL(Utilities.CREATE_FLIGHTS_TABLE);
+        db.execSQL(Utilities.CREATE_FLIGHTSTO_TABLE);
+        db.execSQL(Utilities.CREATE_HAS_TABLE);
+        db.execSQL(Utilities.CREATE_PROMOTION_TABLE);
+        db.execSQL(Utilities.CREATE_ROLE_TABLE);
+        db.execSQL(Utilities.CREATE_ROUTE_TABLE);
+        db.execSQL(Utilities.CREATE_SEATS_TABLE);
+        db.execSQL(Utilities.CREATE_USER_TABLE);
 
     }
 
@@ -50,7 +40,7 @@ public class SQLitehelper extends SQLiteOpenHelper {
         db.execSQL("DROP TABLE IF EXISTS baggage");
         db.execSQL("DROP TABLE IF EXISTS books");
         db.execSQL("DROP TABLE IF EXISTS flights");
-        db.execSQL("DROP TABLE IF EXISTS stopover");
+        db.execSQL("DROP TABLE IF EXISTS stopovers");
         db.execSQL("DROP TABLE IF EXISTS has");
         db.execSQL("DROP TABLE IF EXISTS promotions");
         db.execSQL("DROP TABLE IF EXISTS role");
---
+++ TECAir_Mobile_App/app/src/main/java/com/example/tecairmobile/Utilities/Utilities.java
@@ -0,0 +1,69 @@
+package com.example.tecairmobile.Utilities;
+
+public class Utilities {
+
+    public static final String TABLE_AIRPLANE = "airplane";
+    public static final String TABLE_APTO = "applies_to";
+    public static final String TABLE_BAGGAGE = "baggage";
+    public static final String TABLE_BOOKS = "books";
+    public static final String TABLE_FLIGHTS = "flights";
+    public static final String TABLE_FLIGHTSTO = "stopovers";
+    public static final String TABLE_HAS = "has";
+    public static final String TABLE_PROMOTION = "promotions";
+    public static final String TABLE_ROLE = "role";
+    public static final String TABLE_ROUTE = "route";
+    public static final String TABLE_SEATS = "seats";
+    public static final String TABLE_USER = "user";
+
+    public static final String FIELD_PLATE = "plate";
+    public static final String FIELD_MODEL = "model";
+    public static final String FIELD_FID = "flight_id";
+    public static final String FIELD_PCODE = "promotion_code";
+    public static final String FIELD_FPRICE = "final_price";
+    public static final String FIELD_ID = "id";
+    public static final String FIELD_COLOR = "color";
+    public static final String FIELD_WEIGHT = "weight";
+    public static final String FIELD_UID = "user_id";
+    public static final String FIELD_BGATE = "boarding_gate";
+    public static final String FIELD_PRICE = "price";
+    public static final String FIELD_STATUS = "status";
+    public static final String FIELD_RCODE = "route_code";
+    public static final String FIELD_APLATE = "airplane_plate";
+    public static final String FIELD_STO = "stopover";
+    public static final String FIELD_BAGID = "baggage_id";
+    public static final String FIELD_DESCRIPTION = "description";
+    public static final String FIELD_TITLE = "title";
+    public static final String FIELD_DAY = "day";
+    public static final String FIELD_MONTH = "month";
+    public static final String FIELD_YEAR = "year";
+    public static final String FIELD_DISCOUNT = "discount";
+    public static final String FIELD_NAME = "name";
+    public static final String FIELD_ORIGIN = "origin";
+    public static final String FIELD_DESTINATION = "destination";
+    public static final String FIELD_HOUR = "hours";
+    public static final String FIELD_MINUTES = "minutes";
+    public static final String FIELD_EMAIL = "email";
+    public static final String FIELD_FNAME = "first_name";
+    public static final String FIELD_SNAME = "second_name";
+    public static final String FIELD_FSNAME = "first_surname";
+    public static final String FIELD_SSNAME = "second_surname";
+    public static final String FIELD_PHONE = "phone";
+    public static final String FIELD_UNIVERSITY = "university";
+    public static final String FIELD_STID = "student_id";
+    public static final String FIELD_RNAME = "role_name";
+
+
+    public static final String CREATE_AIRPLANE_TABLE = "CREATE TABLE "+TABLE_AIRPLANE+" ("+FIELD_PLATE+" TEXT, "+FIELD_MODEL+" TEXT)";
+    public static final String CREATE_APTO_TABLE = "CREATE TABLE "+TABLE_APTO+" ("+FIELD_FID+" INTEGER, "+FIELD_PCODE+" INTEGER, "+FIELD_FPRICE+" INTEGER)";
+    public static final String CREATE_BAGGAGE_TABLE = "CREATE TABLE "+TABLE_BAGGAGE+" ("+FIELD_ID+" INTEGER, "+FIELD_COLOR+" TEXT, "+FIELD_WEIGHT+" INTEGER)";
+    public static final String CREATE_BOOKS_TABLE = "CREATE TABLE "+TABLE_BOOKS+" ("+FIELD_UID+" INTEGER, "+FIELD_FID+" INTEGER)";
+    public static final String CREATE_FLIGHTS_TABLE = "CREATE TABLE "+TABLE_FLIGHTS+" ("+FIELD_ID+" INTEGER, "+FIELD_BGATE+" INTEGER, "+FIELD_PRICE+" INTEGER, "+FIELD_STATUS+" BOOLEAN, "+FIELD_RCODE+" INTEGER, "+FIELD_APLATE+" TEXT)";
+    public static final String CREATE_FLIGHTSTO_TABLE = "CREATE TABLE "+TABLE_FLIGHTSTO+" ("+FIELD_FID+" INTEGER, "+FIELD_STO+" TEXT)";
+    public static final String CREATE_HAS_TABLE = "CREATE TABLE "+TABLE_HAS+" ("+FIELD_BAGID+" INTEGER, "+FIELD_UID+" INTEGER, "+FIELD_PRICE+" INTEGER)";
+    public static final String CREATE_PROMOTION_TABLE = "CREATE TABLE "+TABLE_PROMOTION+" ("+FIELD_PCODE+" INTEGER, "+FIELD_DESCRIPTION+" TEXT, "+FIELD_TITLE+" TEXT, "+FIELD_DAY+" INTEGER, "+FIELD_MONTH+" INTEGER, "+FIELD_YEAR+" INTEGER, "+FIELD_DISCOUNT+" INTEGER)";
+    public static final String CREATE_ROLE_TABLE = "CREATE TABLE "+TABLE_ROLE+" ("+FIELD_NAME+" TEXT, "+FIELD_DESCRIPTION+" TEXT)";
+    public static final String CREATE_ROUTE_TABLE = "CREATE TABLE "+TABLE_ROUTE+" ("+FIELD_RCODE+" INTEGER, "+FIELD_ORIGIN+" TEXT, "+FIELD_DESTINATION+" TEXT, "+FIELD_YEAR+" INTEGER, "+FIELD_MONTH+" INTEGER, "+FIELD_DAY+" INTEGER, "+FIELD_HOUR+" INTEGER, "+FIELD_MINUTES+" INTEGER)";
+    public static final String CREATE_SEATS_TABLE = "CREATE TABLE "+TABLE_SEATS+" ("+FIELD_ID+" INTEGER, "+FIELD_DESCRIPTION+" TEXT, state BOOLEAN, "+FIELD_UID+" INTEGER, "+FIELD_APLATE+" TEXT)";
+    public static final String CREATE_USER_TABLE = "CREATE TABLE "+TABLE_USER+" ("+FIELD_ID+" INTEGER, "+FIELD_EMAIL+" TEXT, "+FIELD_FNAME+" TEXT, "+FIELD_SNAME+" TEXT, "+FIELD_FSNAME+" TEXT, "+FIELD_SSNAME+" TEXT, "+FIELD_PHONE+" INTEGER, "+FIELD_UNIVERSITY+" TEXT, "+FIELD_STID+" INTEGER, "+FIELD_RNAME+" TEXT)";
+
+}
