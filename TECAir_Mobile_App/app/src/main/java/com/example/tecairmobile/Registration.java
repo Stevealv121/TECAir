763dcb1bdd45e4762e7bdb6941a6a064c2c76ef8
Dennis Jimenez <alejimenezc@hotmail.com>
2022-04-14 17:24:29 -0600

    [Update] Spinner fix

--- TECAir_Mobile_App/app/src/main/java/com/example/tecairmobile/Registration.java
+++ TECAir_Mobile_App/app/src/main/java/com/example/tecairmobile/Registration.java
@@ -7,14 +7,21 @@ import android.content.Intent;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.Toast;
 
 import com.example.tecairmobile.Utilities.Utilities;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class Registration extends AppCompatActivity {
 
     EditText name, mail, college, studentID,secondname,firstsurname,secondsurname;
+    Spinner student;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,9 +35,33 @@ public class Registration extends AppCompatActivity {
         secondname = (EditText) findViewById(R.id.secondname);
         firstsurname = (EditText) findViewById(R.id.firstsurname);
         secondsurname = (EditText) findViewById(R.id.secondsurname);
+        student = findViewById(R.id.student);
 
-        college.setVisibility(View.INVISIBLE);
-        studentID.setVisibility(View.INVISIBLE);
+        List<String> states = Arrays.asList("Regular","Student");
+        ArrayAdapter adapter = new ArrayAdapter(getApplicationContext(), android.R.layout.simple_spinner_item,states);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        student.setAdapter(adapter);
+
+        student.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                String state = student.getSelectedItem().toString();
+                if(state == "Regular"){
+                    college.setVisibility(View.INVISIBLE);
+                    studentID.setVisibility(View.INVISIBLE);
+                }
+                else{
+                    college.setVisibility(View.VISIBLE);
+                    studentID.setVisibility(View.VISIBLE);
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
     }
 
     public void onClick(View view){
@@ -56,6 +87,7 @@ public class Registration extends AppCompatActivity {
         Long idResult = db.insert(Utilities.TABLE_USER, Utilities.FIELD_ID, values);
 
         Toast.makeText(getApplicationContext(), "Id Registro: " +idResult, Toast.LENGTH_SHORT).show();
+        db.close();
 
     }
 }
