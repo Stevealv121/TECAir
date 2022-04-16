7615a30683d004f6f02c35c2c69189ac501822c4
Steve <stevealv@hotmail.com>
2022-04-16 14:03:20 -0600

    [Add] POST User
    For the API, CORS policy added.

--- TECAir_App_Web/src/app/services/api.service.ts
+++ TECAir_App_Web/src/app/services/api.service.ts
@@ -3,18 +3,24 @@ import { Observable } from 'rxjs/internal/Observable';
 import { LoginI } from '../models/login.interface';
 import { ResponseI } from '../models/response.interface';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { UserI } from '../models/user.interface';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
 
-  url: string = "https://localhost:449636";
+  url: string = "http://localhost:5104/api/";
+  userPath: string = this.url + "User";
 
   constructor(private http: HttpClient) { }
 
   loginByEmail(form: LoginI): Observable<ResponseI> {
-    return this.http.post<ResponseI>(this.url, form)
+    return this.http.post<ResponseI>(this.userPath, form)
 
+  }
+
+  signUp(form: UserI): Observable<ResponseI> {
+    return this.http.post<ResponseI>(this.userPath, form)
   }
 }
