1f9540b42d318d6aad9846c0cde5953a1fd82692
Natalia Gonzalez <natigb@estudiantec.cr>
2022-04-17 10:57:16 -0600

    [Fix] Baggage price

--- TECAir_API/TECAir_API_Data/Repositories/HasRepository.cs
+++ TECAir_API/TECAir_API_Data/Repositories/HasRepository.cs
@@ -70,10 +70,20 @@ namespace TECAir_API_Data.Repositories
         public async Task<bool> InsertUserBaggage(Has usr_baggage)
         {
             var db = dbConnection();
-            var sql = @"
+            var sql = @"SELECT baggage_id
+                       FROM public.""Has"" 
+                       WHERE user_id = @user_id";
+            var temp_result = await db.QueryAsync<string>(sql, new { user_id = usr_baggage.user_id });
+
+            var suitcases = temp_result.ToArray().Length;
+            usr_baggage.price = 0;
+            if (suitcases == 1) usr_baggage.price = 50;
+            else if (suitcases >= 2) usr_baggage.price = 75;
+
+            var sql2 = @"
                         INSERT INTO public.""Has"" (baggage_id,user_id,price)
                         VALUES (@baggage_id,@user_id,@price)";
-            var result = await db.ExecuteAsync(sql, new
+            var result = await db.ExecuteAsync(sql2, new
             {
                 usr_baggage.baggage_id,
                 usr_baggage.user_id,
