6f7ed2949df63a83ae69094c043145d182304935
Steve <stevealv@hotmail.com>
2022-04-20 06:00:33 -0600

    [Fix] Selection seat

--- TECAir_App_Web/src/app/seat-map/seat-map.component.ts
+++ TECAir_App_Web/src/app/seat-map/seat-map.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { elementAt } from 'rxjs';
 import { SeatI } from '../models/seat.interface';
 import { ApiService } from '../services/api.service';
 
@@ -10,24 +11,24 @@ import { ApiService } from '../services/api.service';
 export class SeatMapComponent implements OnInit {
 
   flights: string[] = ["one", "two"];
-  // aircraft: number[][] = [[1, 1, 0, 2, 1, 0, 1], [1, 1, 0, 2, 1, 0, 1],
-  // [1, 1, 0, 2, 1, 0, 1], [1, 1, 0, 2, 1, 0, 1],
-  // [1, 1, 0, 2, 1, 0, 1], [1, 1, 0, 2, 1, 0, 1],
-  // [1, 1, 0, 2, 1, 0, 1], [1, 1, 0, 2, 1, 0, 1],
-  // [1, 1, 0, 2, 1, 0, 1], [1, 1, 0, 2, 1, 0, 1],
-  // [1, 1, 0, 2, 1, 0, 1], [1, 1, 0, 2, 1, 0, 1],
-  // [1, 1, 0, 2, 1, 0, 1], [1, 1, 0, 2, 1, 0, 1],
-  // [1, 1, 0, 2, 1, 0, 1], [1, 1, 0, 2, 1, 0, 1],
-  // [1, 1, 0, 2, 1, 0, 1], [1, 1, 0, 2, 1, 0, 1],
-  // [1, 1, 0, 2, 1, 0, 1], [1, 1, 0, 2, 1, 0, 1],
-  // [1, 1, 0, 2, 1, 0, 1], [1, 1, 0, 2, 1, 0, 1],
-  // [1, 1, 0, 2, 1, 0, 1], [1, 1, 0, 2, 1, 0, 1],
-  // [1, 1, 0, 2, 1, 0, 1], [1, 1, 0, 2, 1, 0, 1],
-  // [1, 1, 0, 2, 1, 0, 1], [1, 1, 0, 2, 1, 0, 1],
-  // [1, 1, 0, 2, 1, 0, 1], [1, 1, 0, 2, 1, 0, 1],
-  // [1, 1, 0, 2, 1, 0, 1], [1, 1, 0, 2, 1, 0, 1],
-  // [1, 1, 0, 2, 1, 0, 1], [1, 1, 0, 2, 1, 0, 1],
-  // [1, 1, 0, 2, 1, 0, 1], [1, 1, 0, 2, 1, 0, 1],]; //36 rows.
+  // aircraft: number[][] = [[0, 0, 0, 2, 0, 0, 0], [0, 0, 0, 2, 0, 0, 0],
+  // [0, 0, 0, 2, 0, 0, 0], [0, 0, 0, 2, 0, 0, 0],
+  // [0, 0, 0, 2, 0, 0, 0], [0, 0, 0, 2, 0, 0, 0],
+  // [0, 0, 0, 2, 0, 0, 0], [0, 0, 0, 2, 0, 0, 0],
+  // [0, 0, 0, 2, 0, 0, 0], [0, 0, 0, 2, 0, 0, 0],
+  // [0, 0, 0, 2, 0, 0, 0], [0, 0, 0, 2, 0, 0, 0],
+  // [0, 0, 0, 2, 0, 0, 0], [0, 0, 0, 2, 0, 0, 0],
+  // [0, 0, 0, 2, 0, 0, 0], [0, 0, 0, 2, 0, 0, 0],
+  // [0, 0, 0, 2, 0, 0, 0], [0, 0, 0, 2, 0, 0, 0],
+  // [0, 0, 0, 2, 0, 0, 0], [0, 0, 0, 2, 0, 0, 0],
+  // [0, 0, 0, 2, 0, 0, 0], [0, 0, 0, 2, 0, 0, 0],
+  // [0, 0, 0, 2, 0, 0, 0], [0, 0, 0, 2, 0, 0, 0],
+  // [0, 0, 0, 2, 0, 0, 0], [0, 0, 0, 2, 0, 0, 0],
+  // [0, 0, 0, 2, 0, 0, 0], [0, 0, 0, 2, 0, 0, 0],
+  // [0, 0, 0, 2, 0, 0, 0], [0, 0, 0, 2, 0, 0, 0],
+  // [0, 0, 0, 2, 0, 0, 0], [0, 0, 0, 2, 0, 0, 0],
+  // [0, 0, 0, 2, 0, 0, 0], [0, 0, 0, 2, 0, 0, 0],
+  // [0, 0, 0, 2, 0, 0, 0], [0, 0, 1, 2, 1, 1, 1]]; //36 rows.
 
   aircraft: number[][] = [];
 
@@ -50,7 +51,6 @@ export class SeatMapComponent implements OnInit {
       if (n == 0) {
         let array: number[] = [];
         for (let is = 0; is < 7; is++) {
-
           if (is == 3) {
             array.push(2);
           } else if (seats[i + is] === undefined) {
@@ -59,104 +59,26 @@ export class SeatMapComponent implements OnInit {
             array.push(1);
           } else if (seats[i + is].state == true) {
             array.push(0);
-
           }
         }
-
-        console.log(array);
         this.aircraft.push(array);
       }
 
     }
   }
 
-  countSeatsUnavailables() {
-
-    let count = 0;
-    let unavailable: number[] = [];
-    this.aircraft.forEach((element) => {
-      element.forEach((j) => {
-        if (j == 1) {
-          count++;
-        }
-      })
-      unavailable.push(count);
-      count = 0;
-    })
-
-    return unavailable;
-  }
-
-  pickSeat(i: any, j: any) {
-    //console.log("selected.")
-    let pos = this.setPos(i, j);
-    // console.log("seatsA:" + this.countSeatsAvailables());
-    // console.log("pos i:" + i);
-    // console.log("pos j:" + j);
-    // console.log("pos: " + pos);
+  pickSeat() {
     let seat = document.getElementsByClassName('square') as HTMLCollectionOf<HTMLElement>;
-    seat[pos].style.backgroundColor = "#213dad";
+    var i;
 
-  }
-
-  setPos(i: any, j: any) {
-    let seatsOccupied = this.countSeatsUnavailables();
-    let col = 6 - seatsOccupied[i];
-    let pos = 0;
-    //let rows = 35;
-    let newJ = this.transformIndex(i, j);
-
-
-    for (let x = 0; x < i; x++) {
-      pos += col;
+    for (i = 0; i < seat.length; ++i) {
+      seat[i].addEventListener('click', changeColor)
     }
-    pos = pos + newJ;
-
-    return pos;
-  }
-  transformIndex(i: any, j: any) {
-    let newJ = 0;
-    let breakError = {};
-    let seatsAvailables = this.countSeatsAvailables();
-
-    try {
-      seatsAvailables[i].forEach((element) => {
-        if (element == j) {
-          throw breakError;
-        } else {
-          newJ++;
-        }
-      })
-
-    } catch (error) {
-
-      if (error !== breakError) throw error;
-
-    }
-
-
-    return newJ;
-
-  }
-  countSeatsAvailables() {
-
-    let jp = 0;
-    let available: number[][] = [];
-    let arr: number[] = [];
-    this.aircraft.forEach((element) => {
-
-      element.forEach((j) => {
-        if (j == 0) {
-          arr.push(jp);
-          jp++;
-        } else { jp++; }
-      })
-      available.push(arr);
-      jp = 0;
-      //i++;
-    })
-
-    return available;
   }
 
 }
+
+function changeColor(this: any) {
+  this.style.backgroundColor = "#213dad";
+}
+
