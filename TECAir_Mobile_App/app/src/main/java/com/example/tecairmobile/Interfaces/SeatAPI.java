21d156aac64c8728f38d223e1f63670004dff92a
Dennis Jimenez <alejimenezc@hotmail.com>
2022-04-24 02:56:46 -0600

    [Update] Complete app functionality

--- TECAir_Mobile_App/app/src/main/java/com/example/tecairmobile/Interfaces/SeatAPI.java
+++ TECAir_Mobile_App/app/src/main/java/com/example/tecairmobile/Interfaces/SeatAPI.java
@@ -5,10 +5,15 @@ import com.example.tecairmobile.entities.Seats;
 import java.util.List;
 
 import retrofit2.Call;
+import retrofit2.http.Body;
 import retrofit2.http.GET;
+import retrofit2.http.PUT;
 
 public interface SeatAPI {
 
     @GET("api/Seat")
     Call<List<Seats>> findS();
+
+    @PUT("api/Seat")
+    Call<Seats> update(@Body Seats seats);
 }
